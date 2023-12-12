bcafcb0ccd26762f7cba286ab7d3b2b6e72510e4
Kyle Suen <51816299+huisui773@users.noreply.github.com>
2023-12-12 21:18:43 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,15 +51,11 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '307c59ae4b82454faf5c7bf8f5cc291f'
+    }
+  ]
 })
